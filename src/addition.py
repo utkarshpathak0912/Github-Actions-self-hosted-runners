027f11145f3f1870df4af6624894b3c2cd923f19
Utkarsh Pathak <74870504+utkarshpathak0912@users.noreply.github.com>
2026-06-22 15:41:49 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,1 +1,8 @@
+def add(a,b):
+  return a+b
 
+def test_add():
+  assert add(1,2) == 3
+  print("Test case 1 passed successfully")
+  assert add(2,3) == 5
+  print("Test case 2 passed successfully")
